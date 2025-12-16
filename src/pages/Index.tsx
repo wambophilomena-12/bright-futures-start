66c1e8a8c704e7d233814f40b797c5f94fb481b1
youngsterpaul <youngsterpaul254@gmail.com>
2025-12-16 19:24:19 +0300

    12

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -514,7 +514,7 @@ const Index = () => {
                               <div 
                                   key={cat.title} 
                                   onClick={() => navigate(cat.path)} 
-                                  className="relative h-24 md:h-40 lg:h-48 cursor-pointer overflow-hidden group rounded-lg" 
+                                  className="relative h-24 md:h-40 lg:h-48 cursor-pointer overflow-hidden group"
                                   style={{
                                       backgroundImage: `url(${cat.bgImage})`,
                                       backgroundSize: 'cover',
